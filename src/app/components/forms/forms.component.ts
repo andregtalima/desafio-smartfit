b8789015cdb2cc23344a75eb235599ad7793ba2a
Andre Fabiano A Lima <andregtalima@gmail.com>
2024-03-12 20:05:15 -0300

    feat: create getUnitService

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css',
 })
@@ -12,9 +14,13 @@ export class FormsComponent implements OnInit {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService
+  ) {}
 
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe((data) => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false,
